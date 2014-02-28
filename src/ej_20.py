46af74dc967173acef5b545139e9b3259794f087
shaila Verona Rodriguez <alu0100823257@ull.edu.es>
2014-02-28 10:31:12 +0000

    hasta el 25

---
+++ src/ej_20.py
@@ -0,0 +1,9 @@
+
+
+a = float(raw_input('Valor de a: '))
+b = float(raw_input('Valor de b: '))
+
+x= -b/a
+
+print 'solucion: ', x
+
---
+++ src/ej_21.py
@@ -0,0 +1,8 @@
+a = float(raw_input('Valor de a: '))
+b = float(raw_input('Valor de b: '))
+
+if a==0:
+  print 'la ecuacion no tiene solucion'
+else:
+  x=-b/a
+  print 'solucion: ', x
